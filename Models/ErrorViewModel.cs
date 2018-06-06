8e7ba3d739fece070722c7a6f45aa4fc825c1add
MAX <822262059@qq.com>
2018-06-06 11:14:57 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminLTETemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
